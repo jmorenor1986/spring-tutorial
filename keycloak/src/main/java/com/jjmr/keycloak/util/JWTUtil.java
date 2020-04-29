17640d5822654fed23c5774eabc3570d8b66a379
jmorenor1986 <johnmorenoing@gmail.com>
2020-04-29 16:46:20 +0200

    Create project with keycloak

---
+++ keycloak/src/main/java/com/jjmr/keycloak/util/JWTUtil.java
@@ -0,0 +1,18 @@
+package com.jjmr.keycloak.util;
+
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class JWTUtil {
+    public static String getJWTToken() {
+        KeycloakAuthenticationToken authentication = (KeycloakAuthenticationToken) SecurityContextHolder.getContext()
+                .getAuthentication();
+
+        KeycloakPrincipal<KeycloakSecurityContext> keycloakPrincipal = (KeycloakPrincipal<KeycloakSecurityContext>) authentication
+                .getPrincipal();
+
+        return keycloakPrincipal.getKeycloakSecurityContext().getTokenString();
+    }
+}
