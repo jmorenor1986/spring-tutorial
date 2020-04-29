57ae1805d42647b178325779946f88aa766a9f72
jmorenor1986 <johnmorenoing@gmail.com>
2020-04-29 10:18:43 +0200

    First commit

---
+++ src/test/java/com/jjmr/spring/pocs/SpringPocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjmr.spring.pocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
