282a38869cef368270ec1731cfba76f52424c062
jmorenor1986 <johnmorenoing@gmail.com>
2020-04-29 11:07:54 +0200

    Create service keepAlive

---
+++ keycloak/src/main/java/com/jjmr/keycloak/KeyCloakApplication.java
@@ -0,0 +1,12 @@
+package com.jjmr.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class KeyCloakApplication extends SpringBootServletInitializer {
+    public static void main(String... args) {
+        SpringApplication.run(KeyCloakApplication.class, args);
+    }
+}
---
+++ keycloak/src/main/java/com/jjmr/keycloak/controller/KeepAliveController.java
@@ -0,0 +1,15 @@
+package com.jjmr.keycloak.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/keepalive")
+public class KeepAliveController {
+
+    @GetMapping("/")
+    public String getAlive() {
+        return "Hi!!, i'm alive";
+    }
+}
